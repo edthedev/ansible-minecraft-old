64f5e7515cb600979b9d897c0d2a6cc78b3c1e99
Edward Delaporte <edthedev@gmail.com>
2014-07-19 10:22:51 -0500

    Curl was not paying nice, so wget.

--- scripts/craftbukkit_download_latest.sh
+++ scripts/craftbukkit_download_latest.sh
@@ -1,1 +1,3 @@
-curl https://dl.bukkit.org/downloads/craftbukkit/get/02389_1.6.4-R2.0/craftbukkit.jar >> ../files/craftbukkit.jar
+#curl https://dl.bukkit.org/downloads/craftbukkit/get/02389_1.6.4-R2.0/craftbukkit.jar > ../files/craftbukkit.jar
+wget https://dl.bukkit.org/downloads/craftbukkit/get/02389_1.6.4-R2.0/craftbukkit.jar -O ../files/craftbukkit.jar
+ls -al ../files
