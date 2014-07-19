eb81754be5f51452f38c592f2bb544819c8b3fb0
Edward Delaporte <edthedev@gmail.com>
2014-07-19 10:19:49 -0500

    Added some common tasks, for quickliness.

---
+++ scripts/craftbukkit_download_latest.sh
@@ -0,0 +1,1 @@
+curl https://dl.bukkit.org/downloads/craftbukkit/get/02389_1.6.4-R2.0/craftbukkit.jar >> ../files/craftbukkit.jar
---
+++ scripts/craftbukkit_latest_web.sh
@@ -0,0 +1,1 @@
+open https://dl.bukkit.org/downloads/craftbukkit/list/rb/
